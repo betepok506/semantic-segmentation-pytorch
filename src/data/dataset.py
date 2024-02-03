fixed the calculation of the cross entropy weights

src/data/dataset.py
@@ -224,8 +224,9 @@ def _create_segmentation_dataset(data_dir: str, config: ConfigurationDataset, na
     return dataset
 
 
-def counting_class_pixels(loader):
+def counting_class_pixels(loader, ignore_index):
     class_weight = None
+    ignore_index_ = ignore_index - 1
     for inputs, targets in loader:
         if class_weight is None:
             class_weight = np.zeros(targets.shape[1])
@@ -234,6 +235,10 @@ def counting_class_pixels(loader):
             counts = np.count_nonzero(targets[:, idx].int(), axis=(0, 1, 2))
             class_weight[idx] += counts
 
+    if ignore_index_ > class_weight.shape[0]:
+        ignore_index_ = class_weight.shape[0] - 1
+
+    class_weight[ignore_index_] = np.inf
     class_weight = np.asarray(class_weight.min() / class_weight)
 
     return class_weight

src/models/engine.py
@@ -232,7 +232,7 @@ class TypeCriterion:
     WEIGHT_CROSS_ENTROPY = 'weight_cross_entropy'
 
 
-def get_criterion(params, data_loader=None, device=None):
+def get_criterion(params, ignore_index, data_loader=None, device=None):
     '''
     Фунция для выбора функции потерь в зависимости от переданных параметров конфигурации
 
@@ -249,7 +249,7 @@ def get_criterion(params, data_loader=None, device=None):
         if data_loader is None:
             raise "For this loss function, you must specify a data loader!"
 
-        weights_classes = counting_class_pixels(data_loader)
+        weights_classes = counting_class_pixels(data_loader, ignore_index)
         # weights_classes = weights_classes.to(device='gpu', dtype=torch.float32)
         criterion = torch.nn.CrossEntropyLoss(weight=torch.from_numpy(weights_classes).to(device))
     elif params.name == TypeCriterion.FOCAL_LOSS: